d0c6e1b34a82b45f5be85eb7f74e3fbe7be3e535
Jamie Christopher Webber <jamiecwebber@gmail.com>
2019-08-23 18:48:17 -0400

    imported soundfile in serve_osc patch

--- serve_osc.py
+++ serve_osc.py
@@ -13,41 +13,45 @@ import argparse
 
 
 def run_network_step(unused_addr, args, *volume):
-	# print("[{0}] ~ {1}".format(args[0], volume))
+	 	print("[{0}] ~ {1}".format(args[0], volume))
 	#print((np.array(volume)*2)-1)
 	
 		update = esn.step((np.array(volume)*2)-1)
 		print(update)
 		count = 0
-	#client.send_message("/wek/nothing", update)
+		client.send_message("/ESN", update)
 		
 
 
 if __name__ == '__main__':
 
-	esn = SimpleESN(n_readout=7, n_components=7, n_inputs=7, input_gain=1, input_sparcity=0.6, damping=0.1, weight_scaling=1.25, sparcity=0.5)
+	esn = SimpleESN(n_readout=7, n_components=7, n_inputs=1, input_gain=1, input_sparcity=0.6, damping=0.1, weight_scaling=1.25, sparcity=0.5)
 
 	print('ESN online')
 	print('Spectral radius: {}'.format(np.max(np.abs(la.eig(esn.weights_)[0]))))
 
 
+	y, sr = librosa.load('VPRYNIMVMT2.wav')
+	y = y/max(y)
+	print('soundfile loaded')
+
 	# for outgoing messages
 	outparser = argparse.ArgumentParser()
-	outparser.add_argument("--ip", default='10.0.1.6', help='The ip of the OSC server')
-	outparser.add_argument("--port", type=int, default=12000, help="The port the OSC server is listening on")
+	outparser.add_argument("--ip", default='127.0.0.1', help='The ip of the OSC server')
+	outparser.add_argument("--port", type=int, default=9000, help="The port the OSC server is listening on")
 	args = outparser.parse_args()
 
 	# for incoming messages
 	inparser = argparse.ArgumentParser()
-	inparser.add_argument("--ip", default='10.0.1.4', help='the ip to listen on')
-	inparser.add_argument("--port", type=int, default=12000, help='The port to liste on')
+	inparser.add_argument("--ip", default='127.0.0.1', help='the ip to listen on')
+	inparser.add_argument("--port", type=int, default=12000, help='The port to listen on')
 	inargs = inparser.parse_args()
 
 	client = udp_client.SimpleUDPClient(args.ip, args.port)
 
 
 	dispatcher = dispatcher.Dispatcher()
-	dispatcher.map("/wek/outputs", run_network_step, "Echo State Network")
+	dispatcher.map("/outputs", run_network_step, "Echo State Network")
 
 	server = osc_server.ThreadingOSCUDPServer(
 		(inargs.ip, inargs.port), dispatcher)
